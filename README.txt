0c154cbcd523f6ce8916b0bb58abf8172b128b91
Benjamin Straw <tehtros@gmail.com>
2012-08-04 02:00:28 -0400

    EGit for eclipse set up! YAY!

---
+++ README.txt
@@ -0,0 +1,4 @@
+A bukkit Plugin for Minecraft Servers:
+This plugin lets you broadcast messages from the server. With style.
+
+Download the plugin http://dev.bukkit.org/server-mods/tcaster/
---
+++ src/tehtros/bukkit/TCaster/TCaster.java
@@ -0,0 +1,101 @@
+package tehtros.bukkit.TCaster;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import tehtros.bukkit.Exceptions.*;
+import tehtros.bukkit.TCastAPI.*;
+
+/**
+ * 
+ * * Le TCaster plugin
+ * 
+ * @author tehtros
+ * 
+ */
+public class TCaster extends JavaPlugin {
+	public static final Logger log = Logger.getLogger("Minecraft");
+	public static TCaster plugin;
+
+	TCastAPI api = new TCastAPI(this);
+
+	public void onEnable() {
+		try {
+			Metrics mets = new Metrics(this);
+			mets.start();
+		} catch(IOException e) {
+			log.warning("[TCaster] Metrics fails to start!");
+		}
+	}
+
+	public void onDisable() {
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("tcast")) {
+			if(sender.hasPermission("tcaster.tcast")) {
+				String text = "";
+				for(String words : args) {
+					if(text.length() > 0)
+						text += " ";
+					text += words;
+				}
+				api.tcast(text);
+			}
+		}
+
+		if(cmd.getName().equalsIgnoreCase("tcastname")) {
+			if(sender.hasPermission("tcaster.name")) {
+				if(args.length != 0) {
+					String newname = "";
+					for(String words : args) {
+						if(!newname.isEmpty()) {
+							newname += " ";
+						}
+						newname += words;
+					}
+
+					try {
+						api.tcastname(newname);
+					} catch(NoNameSupplied e) {
+						sender.sendMessage(ChatColor.GOLD + "[" + ChatColor.DARK_RED + "TCaster" + ChatColor.GOLD + "] " + ChatColor.YELLOW + "You must supply a name! /tcastname [name]");
+					}
+					sender.sendMessage(api.colors(ChatColor.GOLD + "[" + ChatColor.DARK_RED + "TCaster" + ChatColor.GOLD + "] " + ChatColor.DARK_PURPLE + api.getTCastName() + ChatColor.YELLOW + " is ready to chat!"));
+				} else {
+					sender.sendMessage(ChatColor.GOLD + "[" + ChatColor.DARK_RED + "TCaster" + ChatColor.GOLD + "] " + ChatColor.YELLOW + "You must supply a name! /tcastname [name]");
+				}
+			}
+
+		}
+
+		if(cmd.getName().equalsIgnoreCase("tcastcolor")) {
+			if(sender.hasPermission("tcaster.color")) {
+				try {
+					api.tcastcolor(args[0]);
+				} catch(NotValidColor e) {
+					sender.sendMessage(ChatColor.GOLD + "[" + ChatColor.DARK_RED + "TCaster" + ChatColor.GOLD + "] " + ChatColor.YELLOW + "You must supply a valid colorcode! (Ex. &a)");
+				} catch(FailedConfigSave e) {
+					sender.sendMessage(ChatColor.GOLD + "[" + ChatColor.DARK_RED + "TCaster" + ChatColor.GOLD + "] " + ChatColor.YELLOW + "The new config failed to save!");
+				}
+				
+				sender.sendMessage(api.colors(ChatColor.GOLD + "[" + ChatColor.DARK_RED + "TCaster" + ChatColor.GOLD + "] " + ChatColor.YELLOW + "The chat color has been changed to " + api.getChatColor() + "THIS" + ChatColor.YELLOW + "!"));
+			}
+		}
+
+		if(cmd.getLabel().equals("tcastreload")) {
+			if(sender.hasPermission("tcaster.reload")) {
+				if(api.tcastreload()) {
+					sender.sendMessage(ChatColor.GOLD + "[" + ChatColor.DARK_RED + "TCaster" + ChatColor.GOLD + "] " + ChatColor.YELLOW + "has been reloaded!");
+					sender.sendMessage(api.colors(ChatColor.GOLD + "[" + ChatColor.DARK_RED + "TCaster" + ChatColor.GOLD + "] " + ChatColor.DARK_PURPLE + api.getTCastName() + ChatColor.YELLOW + " is ready to chat!"));
+				}
+			}
+		}
+		return true;
+	}
+}
